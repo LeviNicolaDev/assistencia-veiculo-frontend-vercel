2b24e3d859258ce9c3c2b2bbdd94708a4750ef43
Levi Nicola <levinicolas2000@gmail.com>
2025-11-07 17:28:20 -0300

    Update API base URL to new endpoint

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://veiculos-assistencia-a8gyakbscre7axg7.brazilsouth-01.azurewebsites.net/api",
+  //baseURL: "https://veiculos-assistencia-a8gyakbscre7axg7.brazilsouth-01.azurewebsites.net/api",
+  baseURL: "https://veiculos-assistencia2-gmbdgtgrafb4bbfd.brazilsouth-01.azurewebsites.net/api",
   headers: {
     "Content-Type": "application/json",
   },
